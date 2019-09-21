a7bcfc78729764841440fd6dd24e1b84d3dd274f
Demidenko Denys <Demydenkodenys@gmail.com>
2019-09-21 23:57:58 +0300

    add code styling with prismjs

--- src/app/modules/article/containers/article/article.component.ts
+++ src/app/modules/article/containers/article/article.component.ts
@@ -8,8 +8,25 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  language = 'html';
-  content = '<p>wwwdwd<p>';
+  language = 'javascript';
+  content = `const terminal = StripeTerminal.create({
+  onFetchConnectionToken: server.fetchConnectionToken,
+});
+const result = await terminal.discoverReaders();
+const reader = result.discoveredReaders[0];
+await terminal.connectReader(reader);
+
+const item = {
+  description: 'High Growth Handbook',
+  amount: 2000,
+  quantity: 1,
+};
+const cart = {
+  lineItems: [item],
+  currency: 'usd',
+};
+
+terminal.setReaderDisplay({ type: 'cart', cart });`;
 
   constructor() { }
 
