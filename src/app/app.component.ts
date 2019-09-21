e475cb9396167b8d4c1a659873ea04796ad15595
Demidenko Denys <Demydenkodenys@gmail.com>
2019-09-21 22:14:19 +0300

    remove app-component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent implements OnInit {
-
-  constructor() {}
-
-  ngOnInit() {}
-
-}
+export class AppComponent {}
